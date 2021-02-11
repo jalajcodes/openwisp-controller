f8f4da820a18f11de7644972558115ab0fa007a9
Federico Capoano <f.capoano@openwisp.io>
2021-02-11 17:24:43 -0500

    [feature] Added dashboard charts: geo, model, os, hardware

--- openwisp_controller/config/tests/test_app.py
+++ openwisp_controller/config/tests/test_app.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
 
-from openwisp_utils.admin_theme.dashboard import DASHBOARD_CONFIG
+from openwisp_utils.admin_theme.dashboard import DASHBOARD_CHARTS
 
 
 class TestCustomAdminDashboard(TestCase):
-    def test_controller_element_registered(self):
+    def test_config_status_chart_registered(self):
         expected_config = {
             'name': 'Configuration Status',
             'query_params': {
@@ -12,8 +12,22 @@ class TestCustomAdminDashboard(TestCase):
                 'model': 'device',
                 'group_by': 'config__status',
             },
-            'colors': {'applied': 'green', 'modified': 'orange', 'error': 'red'},
+            'colors': {'applied': '#267126', 'modified': '#ffb442', 'error': '#a72d1d'},
+            'labels': {'applied': 'applied', 'error': 'error', 'modified': 'modified'},
         }
-        element_config = DASHBOARD_CONFIG.get(1, None)
-        self.assertNotEqual(element_config, None)
-        self.assertDictEqual(element_config, expected_config)
+        chart_config = DASHBOARD_CHARTS.get(1, None)
+        self.assertIsNotNone(chart_config)
+        self.assertDictEqual(chart_config, expected_config)
+
+    def test_geo_chart_registered(self):
+        chart_config = DASHBOARD_CHARTS.get(2, None)
+        self.assertIsNotNone(chart_config)
+        self.assertIn('labels', chart_config)
+        query_params = chart_config['query_params']
+        self.assertIn('annotate', query_params)
+        self.assertIn('aggregate', query_params)
+        self.assertIn('filters', chart_config)
+        filters = chart_config['filters']
+        self.assertIn('key', filters)
+        self.assertIn('with_geo__sum', chart_config['filters'])
+        self.assertIn('without_geo__sum', chart_config['filters'])
