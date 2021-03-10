1dea9543b1f2ca1a83be5e8beaeca25241c4bb48
Federico Capoano <f.capoano@openwisp.io>
2021-03-10 17:45:49 -0500

    [chores] Reordered tests

--- openwisp_controller/geo/tests/test_apps.py
+++ openwisp_controller/geo/tests/test_apps.py
@@ -3,25 +3,11 @@ from django.test import TestCase
 from openwisp_utils.admin_theme.dashboard import DASHBOARD_CHARTS
 
 
-class TestCustomAdminDashboard(TestCase):
-    def test_config_status_chart_registered(self):
-        expected_config = {
-            'name': 'Configuration Status',
-            'query_params': {
-                'app_label': 'config',
-                'model': 'device',
-                'group_by': 'config__status',
-            },
-            'colors': {'applied': '#267126', 'modified': '#ffb442', 'error': '#a72d1d'},
-            'labels': {'applied': 'applied', 'error': 'error', 'modified': 'modified'},
-        }
-        chart_config = DASHBOARD_CHARTS.get(1, None)
-        self.assertIsNotNone(chart_config)
-        self.assertDictEqual(chart_config, expected_config)
-
+class TestApps(TestCase):
     def test_geo_chart_registered(self):
         chart_config = DASHBOARD_CHARTS.get(2, None)
         self.assertIsNotNone(chart_config)
+        self.assertEqual(chart_config['name'], 'Geographic positioning')
         self.assertIn('labels', chart_config)
         query_params = chart_config['query_params']
         self.assertIn('annotate', query_params)
