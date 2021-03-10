9d902431dd85e907b0200c94e3afbbfd1532a84a
Gagan Deep <pandafy.dev@gmail.com>
2021-03-10 20:43:33 +0530

    [chores] Removed unsed code

--- openwisp_controller/subnet_division/exceptions.py
+++
@@ -1,8 +0,0 @@
-class InvalidVariableName(Exception):
-    """
-    Raised when variable can't be resolved.
-    """
-
-    # NOTE: Separate exceptions for infringing label name, subnet_id or ip_id?
-
-    pass
